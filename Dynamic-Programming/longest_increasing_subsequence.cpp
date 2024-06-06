2ec9091e9ce688db23b10bff8b44d0f4d3a34324
Daniel Huang <74921405+daniel7722@users.noreply.github.com>
2024-06-06 22:52:26 +0800

    longest increasing subsequence

---
+++ Dynamic-Programming/longest_increasing_subsequence.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * This is Dynamic programming approach
+     * Time complexity: O(N^2)
+     * Space complexity: O(N)
+     * @param nums
+     * @return
+     */
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        int ans = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+
+    /**
+     * This is quicker approach with complicated concept which I haven't even grasp fully yet
+     * Time complexity: O(Nlog(N))
+     * Space complexity: O(N)
+     * @param nums
+     * @return
+     */
+    int lengthOfLIS2(vector<int>& nums) {
+        vector<int> ans;
+        for (int x: nums) {
+            if (ans.empty() || x > ans[ans.size()-1]) {
+                ans.push_back(x);
+            } else {
+                auto it = lower_bound(ans.begin(), ans.end(), x);
+                *it = x;
+            }
+        }
+        return ans.size();
+    }
+};
